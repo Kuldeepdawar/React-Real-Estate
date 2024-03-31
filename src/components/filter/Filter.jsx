b7416a13e7a7d3b0a45c2d1f72385a001a901e8e
KuldeepDawar <kuldeepdawar47@gmail.com>
2024-04-01 09:07:16 +1100

    navbar style

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -19,48 +19,38 @@ export default function Filter() {
       <div className="bottom">
         <div className="item">
           <label htmlFor="type">Type</label>
+          <select name="type" id="type">
+            <option value="">Any</option>
+            <option value="buy">Buy</option>
+            <option value="rent">Rent</option>
+          </select>
+        </div>
+        <div className="item">
+          <label htmlFor="property">Property</label>
+          <select name="property" id="property">
+            <option value="">Any</option>
+            <option value="apartment">Apartment</option>
+            <option value="house">House</option>
+            <option value="condo">Condo</option>
+            <option value="landa">Landa</option>
+          </select>
+        </div>{" "}
+        <div className="item">
+          <label htmlFor="minPrice">MinPrice</label>
           <input
-            type="text"
-            id="city"
-            name="city"
-            placeholder="City Location"
+            type="number"
+            id="minPrice"
+            name="minPrice"
+            placeholder="Any"
           />
         </div>{" "}
         <div className="item">
-          <label htmlFor="city">Australia</label>
-          <input
-            type="text"
-            id="city"
-            name="city"
-            placeholder="City Location"
-          />
+          <label htmlFor="maxPrice">Max Price</label>
+          <input type="text" id="maxPrice" name="maxPrice" placeholder="Any" />
         </div>{" "}
         <div className="item">
-          <label htmlFor="city">Australia</label>
-          <input
-            type="text"
-            id="city"
-            name="city"
-            placeholder="City Location"
-          />
-        </div>{" "}
-        <div className="item">
-          <label htmlFor="city">Australia</label>
-          <input
-            type="text"
-            id="city"
-            name="city"
-            placeholder="City Location"
-          />
-        </div>{" "}
-        <div className="item">
-          <label htmlFor="city">Australia</label>
-          <input
-            type="text"
-            id="city"
-            name="city"
-            placeholder="City Location"
-          />
+          <label htmlFor="bedroom">Bedroom</label>
+          <input type="text" id="bedroom" name="bedroom" placeholder="Any" />
         </div>
         <button>
           <img src="/search.png" alt="" />
