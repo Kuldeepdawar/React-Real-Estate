6501fb4d13956ae04f36539d51c5ab59eea7b0e9
KuldeepDawar <kuldeepdawar47@gmail.com>
2024-04-01 09:06:50 +1100

    added design for multiple pages

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,5 +1,45 @@
-import React from "react";
+import "./card.scss";
+import { Link } from "react-router-dom";
 
-export default function Card() {
-  return <div className="card">Card</div>;
+export default function Card({ item }) {
+  return (
+    <div className="card">
+      <Link to={`/${item.id}`} className="imageContainer">
+        <img
+          src="https://www.pexels.com/photo/sheep-on-a-hillside-at-sunset-19845821/"
+          alt=""
+        />
+      </Link>
+      <div className="textContainer">
+        <h2 className="title">
+          <Link to={`/${item.id}`}>{item.title}</Link>
+        </h2>
+        <p className="address">
+          <img src="/pin.png" alt="" />
+          <span>{item.address}</span>
+        </p>
+        <p className="price">{item.price}</p>
+        <div className="bottom">
+          <div className="features">
+            <div className="feature">
+              <img src="/bed.png" alt="" />
+              <span>{item.bedroom} bedroom</span>
+            </div>
+            <div className="feature">
+              <img src="/bath.png" alt="" />
+              <span>{item.bathroom} bathroom</span>
+            </div>
+          </div>
+          <div className="icons">
+            <div className="icon">
+              <img src="/save.png" alt="" />
+            </div>
+            <div className="icon">
+              <img src="/chat.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
